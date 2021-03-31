56adc0d2131c34aed5da5555908728f24a90a633
Jefferson Sofarelli <jmsofarelli@protonmail.com>
2021-03-31 19:29:51 +0100

    v0.1 - Updated code used to fetch a publication

--- pages/[publisher]/[title].js
+++ pages/[publisher]/[title].js
@@ -1,7 +1,8 @@
 import dynamic from 'next/dynamic';
 import Head from "next/head";
 import Preview from "../../components/preview";
-import { getAllPublicationIds, getPublicationData } from "../../lib/publications";
+import { getPublicationData } from "../../lib/publications";
+
 const PdfViewer = dynamic(() => import('../../components/PdfViewer/PdfViewer.js'), { ssr: false });
 
 const API_HOST=process.env.NEXT_PUBLIC_API_HOST;
@@ -15,25 +16,17 @@ export default function Publication({ publicationData }) {
         <meta name="description" content="SourceCheck Publication Preview" />
         <meta name="monetization" content={publicationData.account}></meta>
       </Head>
-      <PdfViewer url={`${API_HOST}${publicationData.pdf_raw.url}`} pageNumber={1} />
+      <PdfViewer url={`${API_HOST}${publicationData.pdf_url}`} pageNumber={1} />
     </Preview>
   );
 }
 
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
   const publicationData = await getPublicationData(params.publisher, params.title);
   console.log('PUBLICATION_DATA', publicationData);
   return {
     props: {
       publicationData: publicationData,
     },
-  };
-}
-
-export async function getStaticPaths() {
-  const paths = await getAllPublicationIds();
-  return {
-    paths,
-    fallback: false,
   };
 }
